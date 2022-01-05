dc1b3d14bde4bf697707c7fbcf14e6fea4abc7f1
Marcelo Palacios <marcelouls@hotmail.com>
2022-01-04 22:52:52 -0300

    expansão de variaveis

---
+++ calculacomexpansao.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Exercicio:calcular Idade usuario a partir
+# da data de nascimmnento e imprimir na tela
+
+
+# Pedir as informações ao usuario
+
+read -p "Digite o seu nome : " NOME
+echo "Nome Cadastrado: ${NOME:?Usuario não forneceu nome\!}"
+read -p "digite a sua data de Nascimento (DD/MM/AAAA) : " DATA_NASCIMENTO
+echo "Data Nasc.: ${DATA_NASCIMENTO:?deve informar data de nascimento\!}"
+
+# Calcular a idade
+
+ANO_DATA_NASCIMENTO=$(echo $DATA_NASCIMENTO | cut -d"/" -f3)
+ANO_ATUAL=$(date +%Y)
+IDADE=$(echo "$ANO_ATUAL-$ANO_DATA_NASCIMENTO" | bc -l)
+
+# Imprimer o resultado na tela
+
+echo "Olá $NOME"
+echo "Este ano vc tem/terá $IDADE anos"
